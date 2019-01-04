ecf537c819c9bea2959b78cfd338dab9e34108bc
Carlo Kneissl <psielisorigin@gmail.com>
2019-01-04 00:02:19 +0000

    modified d url pt2

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export default {
   PORT: process.env.PORT || 8080,
-  DB_URL: process.env.MONGODB_URI,
+  DB_URL: process.env.MONGODB_URI + "/rffserver",
   GRAPHQL_PATH: "/graphql",
   JWT_SECRET: "thisisasecret",
   MAX_LATITUDE_DELTA: 0.02,
